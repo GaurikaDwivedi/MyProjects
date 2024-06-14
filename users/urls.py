80366b1a1923ac1733237bb59764b5aedc581adc
Gaurika Dwivedi <72243047+GaurikaDwivedi@users.noreply.github.com>
2024-06-14 22:11:06 +0530

    Delete users directory

--- users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import Register, LoginView, UserView
-
-urlpatterns = [
-    path('register', Register.as_view()),
-    path('login', LoginView.as_view()),
-    path('user', UserView.as_view())
-]
